acb3ea751f05688cb47efb5e3dcaeb39455a417b
Max Bradey <max@max-thinkpad.localdomain>
2019-09-16 22:34:40 +0300

    Updated firstmod.c and Makefile to provide time monitoring in seconds in the Kernel's log.

--- dk61_shvayuk/lab2_simplest_kernel_modules/src/firstmod.c
+++ dk61_shvayuk/lab2_simplest_kernel_modules/src/firstmod.c
@@ -1,16 +1,25 @@
+/**
+ * Based on the thodnev's code example:
+ * https://github.com/kpi-keoa/kpi-embedded-linux-course/blob/master/demo/lab1/firstmod.c
+ */
 #include <linux/module.h>	// required by all modules
 #include <linux/kernel.h>	// required for sysinfo
 #include <linux/init.h>		// used by module_init, module_exit macros
 #include <linux/jiffies.h>	// where jiffies and its helpers reside
+#include <linux/ktime.h>
+#include <linux/timekeeping.h>
 
-MODULE_DESCRIPTION("Basic module demo: init, deinit, printk, jiffies");
-MODULE_AUTHOR("thodnev");
-MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("Basic module demo (extended): init, deinit, printk, jiffies, current time");
+MODULE_AUTHOR("max_shvayuk");
+MODULE_VERSION("0.228");
 MODULE_LICENSE("Dual MIT/GPL");		// this affects the kernel behavior
 
 static int __init firstmod_init(void)
 {
-	printk(KERN_INFO "Hello, $username!\njiffies = %lu\n", jiffies);
+	struct timespec human_readable_time = {0, 0};
+	getnstimeofday(&human_readable_time);
+
+	printk(KERN_INFO "Hello, $username!\njiffies = %lu\nseconds = %llu\n", jiffies, (unsigned long long)human_readable_time.tv_sec);
 	return 0;
 }
  
